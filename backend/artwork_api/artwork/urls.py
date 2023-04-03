11a27ff493b5b95f8ef18afc71481aeed755ca8a
Saranya2003 <62011240@KMITL.AC.TH>
2023-04-03 15:13:44 +0700

    Modified

--- backend/artwork_api/artwork/urls.py
+++ backend/artwork_api/artwork/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.ListArtwork.as_view()),
     path('<int:pk>/', views.DetailArtwork.as_view()),
-    path('login/',views.Login.as_view()),
 ]
--- backend/artwork_api/artwork_api/settings.py
+++ backend/artwork_api/artwork_api/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'knox.auth.TokenAuthentication',
     ]
 }
 
