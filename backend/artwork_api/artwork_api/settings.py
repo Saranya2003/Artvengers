38c33bcd5fccd78ed338b7f382b3c4bd61eae994
Saranya2003 <62011240@KMITL.AC.TH>
2023-09-22 18:57:26 +0700

    add dj_database_url

--- backend/artwork_api/artwork_api/settings.py
+++ backend/artwork_api/artwork_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import pytz 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,15 +99,18 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'artvengers', 
+#        'USER': 'artvengers',
+#        'PASSWORD': 'Artvengers2023',
+#        'HOST': '127.0.0.1', 
+#        'PORT': '5432',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'artvengers', 
-        'USER': 'artvengers',
-        'PASSWORD': 'Artvengers2023',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
